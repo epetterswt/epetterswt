7fcdf47bf0068fbff80227ec1d67ed87b41d10c8
epetterswt <108840200+epetterswt@users.noreply.github.com>
2025-03-26 16:52:34 -0400

    Deploy update

--- deploy.sh
+++ deploy.sh
@@ -5,5 +5,5 @@ cd dist
 git init
 git add -A
 git commit -m 'New Deployment'
-git push -f git@github.com:epetters/epetterswt.git master:gh-pages
+git push -f git@github.com:epetterswt/epetterswt.git master:gh-pages
 cd -

4fee693119a2ab64783526084a09357997a0780c
epetterswt <108840200+epetterswt@users.noreply.github.com>
2025-04-01 01:49:49 -0400

    Third commit redo

--- src/main.js
+++ src/main.js
@@ -4,7 +4,6 @@ import App from './App.vue'
 createApp(App).mount('#app')
 
 import Vue from 'vue'; 
-import App from './App.vue'; 
 import router from './router'; 
  
 Vue.config.productionTip = false; 
